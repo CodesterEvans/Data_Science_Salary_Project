199fd324568860cc550a131cfbe03ac051136f2b
cody evans <codyevansm@gmail.com>
2021-06-11 09:57:45 -0700

    Web Scraper and execution code

--- Scraper_Execution.py
+++ Scraper_Execution.py
@@ -9,6 +9,9 @@ import Glassdoor_Scraper_Updated as gs
 import pandas as pd 
 path = "C:/Users/como2/Documents/Programming/Data_Science_Salary_Project/chromedriver.exe"
 
-df = gs.get_jobs("data scientist", 5, False, path, 5)
+df = gs.get_jobs("data scientist",350, False, path, 5)
 
 #df_analyst = gs.get_jobs("data analyst", 15, False, path, 5)
+
+df
+#df_analyst
